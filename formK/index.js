ec482a493ab448ec3c072c707e9ef920c300c767
Priyesh3105 <107624403+Priyesh3105@users.noreply.github.com>
2022-12-23 12:33:56 +0530

    Add files via upload

---
+++ formK/index.js
@@ -0,0 +1,9 @@
+function showResult(){
+
+    var uname = document.getElementById("name").value ;
+    var umail = document.getElementById("mail").value ;
+    var ucnum = document.getElementById("cnum").value ;
+
+    
+    document.getElementById("p1").innerHTML=uname+"<br> "+umail+" <br>"+ucnum;
+}
